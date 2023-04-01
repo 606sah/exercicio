63f5d3555931ae3b7548efbd01f80b39453355fd
SrCarlinhos <srcarlinhos2004@gmail.com>
2023-03-31 21:21:48 -0300

    projeto

---
+++ src/CalculaR/main.java
@@ -0,0 +1,30 @@
+
+package CalculaR;
+import java.util.Scanner;
+public class main{
+
+public static void main (String [] args) {
+	
+	Scanner Numero = new Scanner (System.in);
+	int number1;
+	System.out.println("Digite um número");
+ number1 =Numero.nextInt();
+ 
+ Scanner numero2 = new Scanner (System.in);
+ int number2;
+ System.out.println("DIGITA OUTRO");
+number2 = numero2.nextInt();
+
+int soma = (number1 + number2);
+System.out.println("Resultado " + soma);
+
+
+ 
+ 
+	
+}
+
+
+	
+}
+
